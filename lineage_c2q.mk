f97136af4b660aead58344e8154c80faed70d536
Griffin Millender <griffin.millender@copperhead.co>
2020-09-28 16:02:51 -0500

    c2q: Add build fingerprint

--- lineage_c2q.mk
+++ lineage_c2q.mk
@@ -6,3 +6,8 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Product IDs
 PRODUCT_NAME := lineage_c2q
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="c2quew-user 10 QP1A.190711.020 N986U1UEU1ATI5 release-keys"
+
+BUILD_FINGERPRINT := samsung/c2quew/c2q:10/QP1A.190711.020/N986U1UEU1ATI5:user/release-keys
